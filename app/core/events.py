56f75070d10919d18b8c25e0969385c317e49e11
aman <mail@amanskywalker.xyz>
2023-03-27 00:32:14 +0530

    added core settings

---
+++ app/core/events.py
@@ -0,0 +1,43 @@
+from typing import Callable
+
+import asyncpg
+from app.core.settings.app import AppSettings
+from fastapi import FastAPI
+from loguru import logger
+
+
+async def connect_to_db(app: FastAPI, settings: AppSettings) -> None:
+    logger.info("Connecting to PostgreSQL")
+
+    app.state.pool = await asyncpg.create_pool(
+        str(settings.database_url),
+        min_size=settings.min_connection_count,
+        max_size=settings.max_connection_count,
+    )
+
+    logger.info("Connection established")
+
+def create_start_app_handler(
+    app: FastAPI,
+    settings: AppSettings,
+) -> Callable:  # type: ignore
+    async def start_app() -> None:
+        await connect_to_db(app, settings)
+
+    return start_app
+
+
+async def close_db_connection(app: FastAPI) -> None:
+    logger.info("Closing connection to database")
+
+    await app.state.pool.close()
+
+    logger.info("Connection closed")
+
+
+def create_stop_app_handler(app: FastAPI) -> Callable:  # type: ignore
+    @logger.catch
+    async def stop_app() -> None:
+        await close_db_connection(app)
+
+    return stop_app
---
+++ app/core/urls.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+# router.include_router(tags.router, tags=["tags"], prefix="/tags")
